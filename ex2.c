a3eb0f959785c553e5330f2361dfe10ff48556ca
ContardiC <c.contardi@icloud.com>
2021-09-23 16:35:42 +0200

    Ex2 added

--- ex2.c
+++ ex2.c
@@ -1,16 +1,19 @@
 //
 // Created by Carlo Contardi on 23/09/2021.
 //
-/*
+
 #include<stdio.h>
-int F(int *p){
-    *p=*p+4;
-    return *p-2;
+int F(int *p, int x){
+
+    while(x>1){
+        *p=+2;
+        x--;
+    }
+    return *p-x;
 }
 int main(){
     int a=3, b=5, c;
-    c=a+F(&b);
+    c=a+F(&b, a);
     printf("A: %d ; B: %d; C: %d",a,b,c);
     return 0;
 }
-*/
