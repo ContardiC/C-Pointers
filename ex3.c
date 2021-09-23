6f4222c7947079ec0cec12c25cf95ad7f2d17571
ContardiC <c.contardi@icloud.com>
2021-09-23 16:39:38 +0200

    Ex3 added

--- ex3.c
+++ ex3.c
@@ -1,19 +1,20 @@
 //
 // Created by Carlo Contardi on 23/09/2021.
 //
-/*
+
 #include<stdio.h>
-int F(int *p, int x){
-    while(x>1){
-        *p=+2;
-        x--;
-    }
-    return *p-x;
+int F(int *p){
+    *p=*p+4;
+    return *p-2;
+}
+int G(int x, int *p){
+    *p+=4;
+    return 1;
 }
 int main(){
     int a=3, b=5, c;
-    c=a+F(&b, a);
+    c=a+G(F(&b),&a);
     printf("A: %d ; B: %d; C: %d",a,b,c);
     return 0;
 }
-*/
+
